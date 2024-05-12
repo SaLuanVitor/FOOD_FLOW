ade23c7c03f74aeb6ad168925089221edae44ce9
Luan Vitor d' Sá <100245931+SaLuanVitor@users.noreply.github.com>
2024-05-12 15:11:07 -0300

    Atualizar o Pedido.java

--- servidor/springboot/src/main/java/com/example/springboot/models/Pedido.java
+++ servidor/springboot/src/main/java/com/example/springboot/models/Pedido.java
@@ -1,1 +1,25 @@
-Criando entidade Pedido
+package com.example.springboot.models;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "GER_PEDIDO")
+
+public class Pedido implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idMesa;
+    private UUID idFuncionario;
+    private String Status;
+    private DateTime tempo Preparo;
+
+ 
